4ec5bff8cd80d0e4493ba4f58a63c250acbd54ad
sabih.siddiqui <sabih.siddiqui@crowngroup.com.pk>
2023-07-11 17:50:16 +0500

    revert-text sample project

---
+++ text_revert/functionality.js
@@ -0,0 +1,6 @@
+function changeText() {
+    var inputElement = document.getElementById("inputBox");
+    var textElement = document.getElementById("result");
+    var userInput = inputElement.value;
+    textElement.innerHTML = userInput
+}
